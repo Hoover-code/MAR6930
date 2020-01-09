953aef2d3c02d18b627de2e0cebd1153b22629c6
Hoover-code <57542462+Hoover-code@users.noreply.github.com>
2020-01-09 18:00:09 -0500

    Add files via upload

---
+++ ExtractExample.R
@@ -0,0 +1,21 @@
+# Starting a line with a # indicates that the line is a comment and for the program to ignore it
+# It is good coding hygene to comment your code so others can understand what the code is doing
+# This script illustrates how to scrape the same data from the CIA World Fact Book site that we did in Excel
+# The CIA WFB site is: https://www.cia.gov/library/publications/the-world-factbook/fields/335rank.html#UK
+# The code is a modification from this link:
+# http://bradleyboehmke.github.io/2015/12/scraping-html-tables.html
+
+# First, we will load the Packages (Library) necessary to scrape the website
+# This packages need to be "Installed" in R first
+
+library(rvest)
+webpage <- read_html("https://www.cia.gov/library/publications/the-world-factbook/fields/335rank.html")
+tbls <- html_nodes(webpage,"table")
+head(tbls)
+tbls_ls <- webpage %>%
+  html_nodes("table") %>%
+  .[1] %>%
+  html_table(fill = TRUE)
+population <- data.frame(tbls_ls)
+
+
